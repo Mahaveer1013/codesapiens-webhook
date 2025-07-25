dfc12b04688015ca1aa3c3e81bdacf84b3e54b6e
Mahaveer <mahaveer30032005@gmail.com>
2025-07-25 10:49:33 +0530

    updated team

--- scripts/data.js
+++ scripts/data.js
@@ -1,8 +1,14 @@
 export const repositories = [
-
-];
-
-
+       {
+        "name": "git-in-kadhai-077",
+        "collaborators": [
+            "Dhanush2006d",
+            "dhaanish597",
+            "Bhavesh2306"
+        ],
+        "mentor": ["unknowns-007"]
+    }
+]
 export const old_repositories = [
     {
         "name": "git-in-kadhai-001",
@@ -10,7 +16,8 @@ export const old_repositories = [
             "SanjayKumar-hub",
             "vishal-maddy",
             "sivadhasan"
-        ]
+        ],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-002",
@@ -18,7 +25,8 @@ export const old_repositories = [
             "Prasanna-Nadrajan",
             "SUBASH-R-007",
             "MeerNadeemudeen-rec"
-        ]
+        ],
+        "mentor": ["CrystalDarling2000"]
     },
     {
         "name": "git-in-kadhai-003",
@@ -26,7 +34,8 @@ export const old_repositories = [
             "Vsreecharan",
             "nirranjansai",
             "Vishnuvarman007"
-        ]
+        ],
+        "mentor": ["koushikram001"]
     },
     {
         "name": "git-in-kadhai-004",
@@ -34,7 +43,8 @@ export const old_repositories = [
             "Karthickbalaji5",
             "Karthih2",
             "Kabelbas"
-        ]
+        ],
+        "mentor": ["CrystalDarling2000"]
     },
     {
         "name": "git-in-kadhai-005",
@@ -42,7 +52,8 @@ export const old_repositories = [
             "sreeladchayaa",
             "Shivaniibhala",
             "Srivinkumar"
-        ]
+        ],
+        "mentor": ["koushikram001"]
     },
     {
         "name": "git-in-kadhai-006",
@@ -50,7 +61,8 @@ export const old_repositories = [
             "Sowmyalakshmi15",
             "TanushriBalaji",
             "Soameshwaran"
-        ]
+        ],
+        "mentor": ["koushikram001"]
     },
     {
         "name": "git-in-kadhai-007",
@@ -58,7 +70,8 @@ export const old_repositories = [
             "rahulsags",
             "Brijith58",
             "Keekee0207"
-        ]
+        ],
+        "mentor": ["CrystalDarling2000"]
     },
     {
         "name": "git-in-kadhai-008",
@@ -66,7 +79,8 @@ export const old_repositories = [
             "Praveenredm",
             "RevanthkumarSathiasilane",
             "SIDDHU-23052006"
-        ]
+        ],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-009",
@@ -74,7 +88,8 @@ export const old_repositories = [
             "aasif-10",
             "Dev-solder124",
             "harinaath7777"
-        ]
+        ],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-010",
@@ -82,7 +97,8 @@ export const old_repositories = [
             "shallini-16",
             "ClassyMuhi",
             "sivaram7025"
-        ]
+        ],
+        "mentor": ["bharathwaj1002"]
     },
     {
         "name": "git-in-kadhai-011",
@@ -90,7 +106,8 @@ export const old_repositories = [
             "adikesavan",
             "Balaji-9941",
             "gogula0705"
-        ]
+        ],
+        "mentor": ["Mahaveer1013"]
     },
     {
         "name": "git-in-kadhai-012",
@@ -98,7 +115,8 @@ export const old_repositories = [
             "Harikumar4",
             "kapileshn",
             "Vishalraaj5"
-        ]
+        ],
+        "mentor": ["Happday-bot"]
     },
     {
         "name": "git-in-kadhai-013",
@@ -106,7 +124,8 @@ export const old_repositories = [
             "ArghoDutta",
             "Jayanta2004",
             "SubarnoSingh"
-        ]
+        ],
+        "mentor": ["koushikram001"]
     },
     {
         "name": "git-in-kadhai-014",
@@ -114,90 +133,336 @@ export const old_repositories = [
             "Mahalakshmi-0205",
             "Vishva-05",
             "Harini-narayanasamy"
-        ]
+        ],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-015",
-        "collaborators": ["KARTHIKAKRISHNA123", "SivaDurshika-G", "kishoreanandr"]
+        "collaborators": ["KARTHIKAKRISHNA123", "SivaDurshika-G", "kishoreanandr"],
+        "mentor": ["KeeerthanaMG"]
     },
     {
         "name": "git-in-kadhai-016",
-        "collaborators": ["Dharan10", "vjsharan", "adikesavan"]
+        "collaborators": ["Dharan10", "vjsharan", "adikesavan"],
+        "mentor": ["Mahaveer1013"]
     },
     {
         "name": "git-in-kadhai-017",
-        "collaborators": ["MNithishKanna12345", "harshavardhant2005", "taffy-calwin"]
+        "collaborators": ["MNithishKanna12345", "harshavardhant2005", "taffy-calwin"],
+        "mentor": ["Mahaveer1013"]
     },
     {
         "name": "git-in-kadhai-019",
-        "collaborators": ["aashira-fathimaZ", "Arjunkothandan", "jayasurya261"]
+        "collaborators": ["aashira-fathimaZ", "Arjunkothandan", "jayasurya261"],
+        "mentor": ["Happday-bot"]
     },
     {
         "name": "git-in-kadhai-020",
-        "collaborators": ["rahulbalaji1015", "Barath-ash", "ParithiRam"]
+        "collaborators": ["rahulbalaji1015", "Barath-ash", "ParithiRam"],
+        "mentor": ["koushikram001"]
     },
     {
         "name": "git-in-kadhai-021",
-        "collaborators": ["Divaesh07", "Iraiyanbust", "suthekshan"]
+        "collaborators": ["Divaesh07", "Iraiyanbust", "suthekshan"],
+        "mentor": ["KeeerthanaMG"]
     },
     {
         "name": "git-in-kadhai-022",
-        "collaborators": ["Abishakm1507", "ANUSUYA187", "MonishaTSR"]
+        "collaborators": ["Abishakm1507", "ANUSUYA187", "MonishaTSR"],
+        "mentor": ["koushikram001"]
     },
     {
         "name": "git-in-kadhai-023",
-        "collaborators": ["hussainfarhaan", "Sheetal-cell", "Nithish-1311"]
+        "collaborators": ["hussainfarhaan", "Sheetal-cell", "Nithish-1311"],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-024",
-        "collaborators": ["Pranev-123", "faizz-167", "umeshsarathy"]
+        "collaborators": ["Pranev-123", "faizz-167", "umeshsarathy"],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-025",
-        "collaborators": ["FajalurRahman44", "Rithu1410", "Vinodhini111"]
+        "collaborators": ["FajalurRahman44", "Rithu1410", "Vinodhini111"],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-026",
-        "collaborators": ["Athi-m-dev", "kathiravan12-05-2005", "Samvirtha"]
+        "collaborators": ["Athi-m-dev", "kathiravan12-05-2005", "Samvirtha"],
+        "mentor": ["unknowns-007"]
     },
     {
         "name": "git-in-kadhai-027",
-        "collaborators": ["pavithran-devs", "pavithran-devs", "Anu-ska"]
+        "collaborators": ["pavithran-devs", "pavithran-devs", "Anu-ska"],
+        "mentor": ["Jhsoul69"]
     },
     {
         "name": "git-in-kadhai-028",
-        "collaborators": ["boobesh2912", "Mohan-Balaji"]
+        "collaborators": ["boobesh2912", "Mohan-Balaji"],
+        "mentor": ["Happday-bot"]
     },
     {
         "name": "git-in-kadhai-029",
-        "collaborators": ["aninthitha-medai", "Meekio", "badhri12"]
+        "collaborators": ["aninthitha-medai", "Meekio", "badhri12"],
+        "mentor": ["Jhsoul69"]
     },
     {
         "name": "git-in-kadhai-030",
-        "collaborators": ["tejaswi-deva", "kapileshn", "aishwarya04082"]
+        "collaborators": ["tejaswi-deva", "kapileshn", "aishwarya04082"],
+        "mentor": ["Happday-bot"]
     },
     {
         "name": "git-in-kadhai-031",
-        "collaborators": ["Harikumar4", "Prakash1210", "Jhsoul69"]
+        "collaborators": ["Harikumar4", "Prakash1210", "Jhsoul69"],
+        "mentor": ["Jhsoul69"]
     },
     {
         "name": "git-in-kadhai-032",
-        "collaborators": ["A-Muthurama", "InzeeraZakariah", "YASHVANTH69"]
+        "collaborators": ["A-Muthurama", "InzeeraZakariah", "YASHVANTH69"],
+        "mentor": ["Jhsoul69"]
     },
     {
         "name": "git-in-kadhai-033",
-        "collaborators": ["suganesh007", "Madhanmetha", "Rakshiiitha"]
+        "collaborators": ["suganesh007", "Madhanmetha", "Rakshiiitha"],
+        "mentor": ["bharathwaj1002"]
     },
     {
         "name": "git-in-kadhai-034",
-        "collaborators": ["30sandz", "arun4374", "Shahscode"]
+        "collaborators": ["30sandz", "arun4374", "Shahscode"],
+        "mentor": ["bharathwaj1002"]
     },
     {
         "name": "git-in-kadhai-035",
-        "collaborators": ["in1yan", "Mukky2005", "av11g"]
+        "collaborators": ["in1yan", "Mukky2005", "av11g"],
+        "mentor": ["bharathwaj1002"]
     },
     {
         "name": "git-in-kadhai-036",
-        "collaborators": ["deepan-codes", "sakthi-r-developer", "dhinakar2310"]
+        "collaborators": ["deepan-codes", "sakthi-r-developer", "dhinakar2310"],
+        "mentor": ["KeeerthanaMG"]
+    },
+    {
+        "name": "git-in-kadhai-037",
+        "collaborators": ["auzzton", "pravinchris", "Ponmathi-07"],
+        "mentor": ["Jhsoul69"]
+    },
+    {
+        "name": "git-in-kadhai-038",
+        "collaborators": ["Maghima2302", "meenakshiganapathydevi@gmail.com"],
+        "mentor": ["CrystalDarling2000"]
+    },
+    {
+        "name": "git-in-kadhai-039",
+        "collaborators": ["gopikaveera", "Arjunkothandan", "AKSHAYA-GIT004-12"],
+        "mentor": ["bharathwaj1002"]
+    },
+    {
+        "name": "git-in-kadhai-040",
+        "collaborators": ["rathinagowry", "akshith123-dev", "Sajin-Reyans"],
+        "mentor": ["Happday-bot"]
+    },
+    {
+        "name": "git-in-kadhai-041",
+        "collaborators": ["pradeepdevs140", "nagappan79", "YOGACHARANM"],
+        "mentor": ["Mahaveer1013"]
+    },
+    {
+        "name": "git-in-kadhai-042",
+        "mentor": ["CrystalDarling2000"],
+        "collaborators": [
+            "SurweeshSP",
+            "vasanthakumar2756"
+        ]
+    },
+    {
+        "name": "git-in-kadhai-043",
+        "mentor": ["Happday-bot"],
+        "collaborators": ["Strange-0007", "KaruneshT1", "PraveenVenktateshNA"]
+    },
+    {
+        "name": "git-in-kadhai-044",
+        "mentor": ["bharathwaj1002"],
+        "collaborators": ["Deepak-adev", "Asrithrajeshv", "auhariprasath"]
+    },
+    {
+        "name": "git-in-kadhai-045",
+        "mentor": ["koushikram001"],
+        "collaborators": ["jayashreekannan", "Keerthiga7904", "Kiran11313"]
+    },
+    {
+        "name": "git-in-kadhai-046",
+        "mentor": ["koushikram001"],
+        "collaborators": ["Lakshmideepika07", "KeerthanaJayasudha", "kannan064"]
+    },
+    {
+        "name": "git-in-kadhai-047",
+        "mentor": ["KeeerthanaMG"],
+        "collaborators": ["sriyaanchita", "swethanagaraj2007", "Vaishu-vs19"]
+    },
+    {
+        "name": "git-in-kadhai-048",
+        "mentor": ["Mahaveer1013"],
+        "collaborators": ["PuvanesuR", "lalshivaan45", "Sheik-Noordeen"]
+    },
+    {
+        "name": "git-in-kadhai-049",
+        "mentor": ["Jhsoul69"],
+        "collaborators": ["Sabharishraja", "Shamvruth163", "Sricharan-rec"]
+    },
+    {
+        "name": "git-in-kadhai-050",
+        "mentor": ["bharathwaj1002"],
+        "collaborators": ["Yeshwanti", "vedhavilasini", "Shivani-Varma1408"]
+    },
+    {
+        "name": "git-in-kadhai-051",
+        "mentor": ["bharathwaj1002"],
+        "collaborators": ["pravinraj213", "suriyadns", "SreemanNaibal"]
+    },
+    {
+        "name": "git-in-kadhai-052",
+        "mentor": ["Jhsoul69"],
+        "collaborators": ["rahulsags", "Brijith58", "Keekee0207"]
+    },
+    {
+        "name": "git-in-kadhai-053",
+        "mentor": ["Jhsoul69"],
+        "collaborators": ["rahulbalaji1015", "Naveenkumar0502", "mukund0103"]
+    },
+    {
+        "name": "git-in-kadhai-054",
+        "mentor": ["Mahaveer1013"],
+        "collaborators": ["kingpaviyonmanova", "KarthiksahE", "mishal-2408"]
+    },
+    {
+        "name": "git-in-kadhai-055",
+        "mentor": ["ashikpy"],
+        "collaborators": ["230701262", "230701264", "230701259"]
+    },
+    {
+        "name": "git-in-kadhai-056",
+        "mentor": ["KeeerthanaMG"],
+        "collaborators": ["Arunachalam02-A", "ameenbasith938", "231001018-Arun"]
+    },
+    {
+        "name": "git-in-kadhai-057",
+        "mentor": ["KeeerthanaMG"],
+        "collaborators": ["Kmkishore05", "Abhiram-vadapalli", "anbuselvam2004"]
+    },
+    {
+        "name": "git-in-kadhai-058",
+        "mentor": ["Jhsoul69"],
+        "collaborators": ["Priyankhar", "Sanjay-Vasudhevan", "Prateekshaas"]
+    },
+    {
+        "name": "git-in-kadhai-059",
+        "mentor": ["ashikpy"],
+        "collaborators": ["Janani231801065", "KrithikaMA", "Mahalakshmisrinuvasan"]
+    },
+    {
+        "name": "git-in-kadhai-060",
+        "mentor": ["Jhsoul69"],
+        "collaborators": ["Harini7-S", "Abhinavu-Prasad", "Dharshana-231801032"]
+    },
+    {
+        "name": "git-in-kadhai-061",
+        "mentor": ["ashikpy"],
+        "collaborators": ["Mathan-dotcom", "Marieshalini", "pavitara30"]
+    },
+    {
+        "name": "git-in-kadhai-062",
+        "mentor": ["CrystalDarling2000"],
+        "collaborators": ["Dejaswini", "Doneeswaran", "RashmiRavikumar1985"]
+    },
+    {
+        "name": "git-in-kadhai-063",
+        "mentor": ["Happday-bot"],
+        "collaborators": ["Priyanexodus", "it-s-sai-0", "LSSDaniel"]
+    },
+    {
+        "name": "git-in-kadhai-064",
+        "mentor": ["bharathwaj1002"],
+        "collaborators": ["Kishorepec", "raj1xx", "SABESHALIN"]
+    },
+    {
+        "name": "git-in-kadhai-065",
+        "mentor": ["Jhsoul69"],
+        "collaborators": ["shaphia", "DR-1029", "irudhayarose"]
+    },
+    {
+        "name": "git-in-kadhai-066",
+        "mentor": ["ashikpy"],
+        "collaborators": ["pravinraj213", "suriyadns", "SreemanNaibal"]
+    },
+    {
+        "name": "git-in-kadhai-067",
+        "mentor": ["koushikram001"],
+        "collaborators": ["LeveaD", "StephClar", "Sam-Rapheal-22"]
+    },
+    {
+        "name": "git-in-kadhai-068",
+        "mentor": ["koushikram001"],
+        "collaborators": ["Avishnavi", "BHAGYALAKSHMIMAHESHBABU", "nil"]
+    },
+    {
+        "name": "git-in-kadhai-069",
+        "mentor": ["koushikram001"],
+        "collaborators": ["auhariprasath", "Deepak-adev", "Asrithrajeshv"]
+    },
+    {
+        "name": "git-in-kadhai-070",
+        "mentor": ["ashikpy"],
+        "collaborators": ["IsaacPerinbaraj-A", "Mithul-P", "Maheendran2006"]
+    },
+    {
+        "name": "git-in-kadhai-071",
+        "mentor": ["Mahaveer1013"],
+        "collaborators": ["MariaSherlin-A", "prakash-1006", "poojamarcus"]
+    },
+    {
+        "name": "git-in-kadhai-072",
+        "mentor": ["bharathwaj1002"],
+        "collaborators": ["santhosheyzz", "suguru-git", "DILIPKUMAR2006"]
+    },
+    {
+        "name": "git-in-kadhai-073",
+        "mentor": ["bharathwaj1002"],
+        "collaborators": ["H4des1337"]
+    },
+    {
+        "name": "git-in-kadhai-074",
+        "collaborators": [
+            "Dhanush2006d",
+            "dhaanish597",
+            "Bhavesh2306"
+        ],
+        "mentor": ["unknowns-007"]
+    },
+    {
+        "name": "git-in-kadhai-075",
+        "collaborators": [
+            "VaishuSidhu",
+            "supravat011",
+            "Lakshu11"
+        ],
+        "mentor": ["unknowns-007"]
+    },
+    {
+        "name": "git-in-kadhai-076",
+        "collaborators": [
+            "xnaveenx",
+            "datchayaniraja",
+            "imtheol"
+        ],
+        "mentor": ["unknowns-007"]
     }
+    // {
+    //     "name": "git-in-kadhai-069",
+    //     "mentor": ["Jhsoul69"],
+    //     "collaborators": [
+    //         "https://www.linkedin.com/in/meenakshi-ganapathy-78440a2a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    //         "https://www.linkedin.com/in/meenakshi-ganapathy-78440a2a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    //         "https://www.linkedin.com/in/aswini-g-3b49112a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+    //     ]
+    // },
 ];
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Octokit } = require('@octokit/rest');
-const { repositories } = require('./data');
+const { old_repositories } = require('./data');
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
@@ -9,28 +9,57 @@ const org = process.env.ORG_NAME; // Add this in your .env file
 async function createRepositories() {
     for (const repo of repositories) {
         try {
-            const { data } = await octokit.repos.createInOrg({
-                org,
-                name: repo.name,
-                private: true,
-                auto_init: true,
+            await octokit.repos.get({
+                owner: org,
+                repo: repo.name,
             });
-            console.log(`✅ Created: ${data.full_name}`);
-        } catch (err) {
-            console.error(`❌ Failed to create ${repo}: ${err.response?.data?.message || err.message}`);
+            console.log(`⚠️ Repo ${repo.name} already exists, skipping creation.`);
+            continue; // skip to next repo
+        } catch (error) {
+            if (error.status !== 404) throw error; // real error
+            try {
+                const { data } = await octokit.repos.createInOrg({
+                    org,
+                    name: repo.name,
+                    private: true,
+                    auto_init: true,
+                });
+                console.log(`✅ Created: ${data.full_name}`);
+            } catch (err) {
+                const message = err.response?.data?.message || err.message;
+
+                if (message.includes("name already exists")) {
+                    console.log(`ℹ️ Skipped: ${repo.name} (already exists)`);
+                    continue;
+                }
+
+                console.error(`❌ Failed to create ${repo.name}: ${message}`);
+            }
         }
     }
 }
 
-
-
 const webhook_url = process.env.WEBHOOK_URL;
 const webhook_secret = process.env.GITHUB_WEBHOOK_SECRET;
-
 
 async function configureWebhooks() {
     for (const repo of repositories) {
         try {
+            // Get all existing webhooks for the repo
+            const { data: hooks } = await octokit.repos.listWebhooks({
+                owner: org,
+                repo: repo.name,
+            });
+
+            // Check if the webhook URL already exists
+            const alreadyExists = hooks.some(hook => hook.config?.url === webhook_url);
+
+            if (alreadyExists) {
+                console.log(`⚠️ Webhook already exists for ${repo.name}, skipping.`);
+                continue;
+            }
+
+            // Create the webhook
             await octokit.repos.createWebhook({
                 owner: org,
                 repo: repo.name,
@@ -38,7 +67,7 @@ async function configureWebhooks() {
                     url: webhook_url,
                     content_type: 'json',
                     secret: webhook_secret,
-                    insecure_ssl: '0'
+                    insecure_ssl: '0',
                 },
                 events: [
                     'push',
@@ -47,8 +76,8 @@ async function configureWebhooks() {
                     'pull_request_review_comment',
                     'issues',
                     'issue_comment',
-                    'create',        // for branch or tag creation
-                    'delete',        // for branch or tag deletion
+                    'create',
+                    'delete',
                     'fork',
                     'star',
                     'workflow_run',
@@ -56,17 +85,18 @@ async function configureWebhooks() {
                 ],
                 active: true
             });
-            console.log(`✅ Webhook confwigured for ${repo.name}`);
+
+            console.log(`✅ Webhook configured for ${repo.name}`);
         } catch (err) {
-            console.error(`❌ Failed to configure webhook for ${repo.name}: ${err.message}`);
+            console.error(`❌ Failed to configure webhook for ${repo.name}: ${err.response?.data?.message || err.message}`);
         }
     }
 }
 
 async function addCollaborators() {
-    for (const repo of repositories) {
+    for (const repo of old_repositories) {
         const repoName = repo.name;
-        const users = repo.collaborators || [];
+        const users = repo.mentor || [];
 
         for (const username of users) {
             try {
@@ -83,16 +113,10 @@ async function addCollaborators() {
         }
     }
 }
-createRepositories().then(() => {
-    configureWebhooks().then(() => {
+// createRepositories().then(() => {
+//     configureWebhooks().then(() => {
         addCollaborators().then(() => {
             console.log("✅ All Done");
-        }).catch((err) => {
-            console.log(err);
         })
-    }).catch((err) => {
-        console.log(err);
-    })
-}).catch((err) => {
-    console.log(err);
-})
+//     })
+// })
